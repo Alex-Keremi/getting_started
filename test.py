87436c812d43d6f40459d5b5ebc32de789f776f6
Alex Keremi <getalexkeremi@gmail.com>
2020-04-12 16:36:39 -0400

    First python and html commit

---
+++ test.py
@@ -0,0 +1,41 @@
+
+# print("Hello. Getting started with Git!")
+
+"""
+# lambda eg1  single variable
+
+x = lambda a: a + 10
+print(x(7))
+
+# lambda eg2  more than one variable
+x = lambda a,b : a*b
+print(x(5, 3))
+
+
+# lambda eg3  using a def function with lambda
+
+def func(n):
+    return lambda a: a*n
+
+x = func(10)
+
+for i in range(1, 11):
+    print(x(i))
+
+
+# lambda eg4  using filter with lambda to get odd numbers in a list
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ]
+
+oddlist = list(filter(lambda a: a % 2 != 0, list1))
+print(oddlist)
+
+
+# lambda eg5  using map with lambda
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ]
+
+list2 = tuple(map(lambda a: a*3, list1))
+
+print(list2)
+"""
